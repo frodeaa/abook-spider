293561a8db2bcb237eadc35f6c668da83fad4b7f
Frode Aannevik <frode.aa@gmail.com>
2015-01-05 19:15:39 +0100

    start on a spider for audiobookbay

---
+++ audiobookbay.py
@@ -0,0 +1,9 @@
+import scrapy
+
+class Audiobookbay(scrapy.Spider):
+				name = 'Audiobookbat'
+
+				def __init__(self, *args, **kwargs):
+								super(Audiobookbay, self).__init__(*args, **kwargs)
+
+
