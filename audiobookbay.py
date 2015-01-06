73b4c25a6fba5f843f9af62262d0b80491d1b57c
Frode Aannevik <frode.aa@gmail.com>
2015-01-06 21:29:21 +0100

    add seeds and peers field to BookItem

--- audiobookbay.py
+++ audiobookbay.py
@@ -9,6 +9,9 @@ class BookItem(scrapy.Item):
     format = scrapy.Field()
     bitrate = scrapy.Field()
     is_abridged = scrapy.Field()
+    peers = scrapy.Field()
+    seeds = scrapy.Field()
+
 
 class Audiobookbay(scrapy.Spider):
     name = 'Audiobookbat'
@@ -28,8 +31,12 @@ class Audiobookbay(scrapy.Spider):
                 for v in resp.xpath(query).extract():
                     item[key] = v
 
-            scrapy.log.msg(">>P> %s " % resp.xpath("//tr[td[text()='Peers:']]/td/text()").extract())
-            scrapy.log.msg(">>S %s " % resp.xpath("//tr[td[text()='Seeds:']]/td/text()").extract())
+            kv = {"peers" : "Peers:", "seeds" : "Seeds:"}
+            for k in kv:
+                query = "//tr[td[text()='%s']]/td/text()" % kv[k]
+                for v in resp.xpath(query).extract():
+                    item[k] = v
+
             scrapy.log.msg(">>CFS %s " % resp.xpath("//tr[td[text()='Combined File Size:']]/td/text()").extract())
             scrapy.log.msg(">>CD %s " % resp.xpath("//tr[td[text()='Completed Downloads:']]/td/text()").extract())
             scrapy.log.msg(">>D %s " % resp.xpath("//a[starts-with(@href, '/download.php?f')]/@href").extract())
